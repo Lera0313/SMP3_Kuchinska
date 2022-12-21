492d6159be5ab693875a54a339426f264520244f
Lera0313 <justdragodwarf@gmail.com>
2022-12-21 20:08:45 +0200

    Lab3 Kuchinska

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Lab3 Kuchinska Valeria
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const objectId = require("mongodb").ObjectID;
@@ -16,87 +17,87 @@ app.use(express.static(__dirname + "/public"));
 mongoClient.connect(function(err, client){
     if(err) return console.log(err);
     dbClient = client;
-    app.locals.collection = client.db("songsdb")
-    .collection("songs");
+    app.locals.collection = client.db("filmsdb")
+    .collection("films");
     app.listen(3000, function(){
         console.log("Сервер чекає на підключення...");
     });
 });
 
-// для отримання книг
-app.get("/api/songs", function(req, res){
+// для отримання films
+app.get("/api/films", function(req, res){
         
     const collection = req.app.locals.collection;
-    collection.find({}).toArray(function(err, tracks){
+    collection.find({}).toArray(function(err, films){
          
         if(err) return console.log(err);
-        res.send(tracks)
+        res.send(films)
     });
      
 });
-// для отримання книги
-app.get("/api/songs/:id", function(req, res){
+// для отримання film
+app.get("/api/films/:id", function(req, res){
         
     const id = new objectId(req.params.id);
     const collection = req.app.locals.collection;
-    collection.findOne({_id: id}, function(err, track){
+    collection.findOne({_id: id}, function(err, film){
                
         if(err) return console.log(err);
-        res.send(track);
+        res.send(film);
     });
 });
 
-// для додавання книги в базу даних
-app.post("/api/songs", jsonParser, function (req, res) {
+// для додавання film в базу даних
+app.post("/api/films", jsonParser, function (req, res) {
        
     if(!req.body) return res.sendStatus(400);
        
-    const trackName = req.body.name;
-    const trackAuthor = req.body.author;
-    const trackGenre = req.body.genre;
-    const trackDate = req.body.date; 
-    const track = {name: trackName, author: trackAuthor, 
-        genre: trackGenre, date: trackDate};
+    const filmName = req.body.name;
+    const filmDirector = req.body.director;
+    const filmGenre = req.body.genre;
+    const filmDate = req.body.date; 
+    const film = {name: filmName, director: filmDirector, 
+        genre: filmGenre, date: filmDate};
        
     const collection = req.app.locals.collection;
-    collection.insertOne(track, function(err, result){
+    collection.insertOne(film, function(err, result){
                
         if(err) return console.log(err);
-        res.send(track);
+        res.send(film);
     });
 });
 
-// для вилучення книги із бази даних
-app.delete("/api/songs/:id", function(req, res){
+// для вилучення film із бази даних
+app.delete("/api/films/:id", function(req, res){
         
     const id = new objectId(req.params.id);
     const collection = req.app.locals.collection;
     collection.findOneAndDelete({_id: id}, function(err, result){
                
         if(err) return console.log(err);    
-        let track = result.value;
-        res.send(track);
+        let film = result.value;
+        res.send(film);
     });
 });
 
-// для оновлення інформації про книгу
-app.put("/api/songs", jsonParser, function(req, res){
+// для оновлення інформації про film
+app.put("/api/films", jsonParser, function(req, res){
         
     if(!req.body) return res.sendStatus(400);
     const id = new objectId(req.body.id);
-    const trackName = req.body.name;
-    const trackAuthor = req.body.author;
-    const trackGenre = req.body.genre;
-    const trackDate = req.body.date; 
+    const filmName = req.body.name;
+    const filmDirector = req.body.director;
+    const filmGenre = req.body.genre;
+    const filmDate = req.body.date; 
        
     const collection = req.app.locals.collection;
-    collection.findOneAndUpdate({_id: id}, { $set: {name: trackName, 
-        author: trackAuthor, genre: trackGenre, date: trackDate}},
+    collection.findOneAndUpdate({_id: id}, { $set: {name: filmName, 
+        director: filmDirector, genre: filmGenre, date: filmDate}},
          {returnOriginal: false },function(err, result){
                
         if(err) return console.log(err);     
-        const track = result.value;
-        res.send(track);
+        const film = result.value;
+        res.send(film);
     });
 });
  
